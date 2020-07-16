67d94028a258a78f2f795b7d5c9e144e99b316a2
Albert Fleming Lukito <albert_fleminglukito@yahoo.com>
2020-07-16 11:54:00 +0700

    initial commit"

---
+++ app/src/main/java/com/fleming/tiket/ui/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.fleming.tiket.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fleming.tiket.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
